10d3e872211984c3b15b88a955059aaf71452f64
Sam Wu <swu@nait.ca>
2018-04-16 14:02:15 -0600

    Added more JUnit Test Case on RESTful web services.

--- dmit2015-demos/src/test/java/northwind/rest/NorthwindRESTServiceTest.java
+++ dmit2015-demos/src/test/java/northwind/rest/NorthwindRESTServiceTest.java
@@ -22,6 +22,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import northwind.entity.Category;
+import northwind.entity.Region;
+import northwind.entity.Territory;
 
 public class NorthwindRESTServiceTest {
 
@@ -57,6 +59,97 @@ public class NorthwindRESTServiceTest {
 		this.client = this.client.register(new BasicAuthentication("admin01", "Password2015"));
 		
 //		this.client = ClientBuilder.newBuilder().sslContext(sc).build().register(new Authenticator("admin01", "Password2015"));
+		
+	}
+	
+	@Test
+	public void testRegionCRUD() {
+		
+		// create a new Region 
+		Region newRegion = new Region();
+		newRegion.setRegionDescription("JUnit Test Region");
+		
+		WebTarget resource = client.target(BASE_URI).path("regions");
+		
+		// call the web service method to create a new region entity
+		Response response = resource.request(MediaType.APPLICATION_JSON).post( Entity.entity(newRegion, MediaType.APPLICATION_JSON) );
+		assertEquals(200, response.getStatus());
+		// Read the regionId value included in the response
+		Integer regionId = response.readEntity(Integer.class);
+		response.close();
+		
+		// call the web service method to get the entity we just added 
+		Region addedRegion = resource.path("{id}").resolveTemplate("id", regionId).request().get(Region.class);
+		assertNotNull(addedRegion);
+		assertEquals(regionId.intValue(), addedRegion.getRegionID());
+		assertEquals(newRegion.getRegionDescription(), addedRegion.getRegionDescription());
+		
+		// change the region name and description with new values
+		addedRegion.setRegionDescription("UPDATED JUnit Test Region");
+		// call the web service method to update the region entity
+		response = resource.request(MediaType.APPLICATION_JSON).put( Entity.json(addedRegion) );
+		assertEquals(200, response.getStatus());
+		response.close();
+		
+		// call the web service method to get the entity we just added 
+		Region updatedRegion = resource.path("{id}").resolveTemplate("id", regionId).request().get(Region.class);
+		assertNotNull(updatedRegion);
+		assertEquals(addedRegion.getRegionID(), updatedRegion.getRegionID());
+		assertEquals(addedRegion.getRegionDescription(), updatedRegion.getRegionDescription());
+		
+		// call the RESTful web service to delete the region entity
+		response = resource.path("{id}").resolveTemplate("id", regionId).request(MediaType.APPLICATION_JSON).delete();
+		assertEquals(200, response.getStatus());
+		response.close();		
+		
+	}
+	
+	@Test
+	public void testTerritoryCRUD() {
+		
+		// create a new Territory 
+		Territory newTerritory = new Territory();
+		String newTerritoryId = "JUNIT01";
+		newTerritory.setTerritoryID(newTerritoryId);
+		newTerritory.setTerritoryDescription("JUnit Test Territory");
+		
+		WebTarget regionResource = client.target(BASE_URI).path("regions");
+		// call the web service method to get a Region with a regionId of 3
+		Region northernRegion = regionResource.path("{id}").resolveTemplate("id", 3).request().get(Region.class);
+		assertNotNull(northernRegion);
+		newTerritory.setRegion(northernRegion);
+				
+		WebTarget territoryResource = client.target(BASE_URI).path("territories");
+		
+		// call the web service method to create a new Territory entity
+		Response response = territoryResource.request(MediaType.APPLICATION_JSON).post( Entity.entity(newTerritory, MediaType.APPLICATION_JSON) );
+		assertEquals(200, response.getStatus());
+		response.close();
+		
+		// call the web service method to get the entity we just added 
+		Territory addedTerritory = territoryResource.path("{id}").resolveTemplate("id", newTerritoryId).request().get(Territory.class);
+		assertNotNull(addedTerritory);
+		assertEquals(newTerritoryId, addedTerritory.getTerritoryID());
+		assertEquals(newTerritory.getTerritoryDescription(), addedTerritory.getTerritoryDescription());
+		assertEquals(northernRegion.getRegionID(), newTerritory.getRegion().getRegionID());
+		
+		// change the region name and description with new values
+		addedTerritory.setTerritoryDescription("UPDATED JUnit Test Territory");
+		// call the web service method to update the region entity
+		response = territoryResource.request(MediaType.APPLICATION_JSON).put( Entity.json(addedTerritory) );
+		assertEquals(200, response.getStatus());
+		response.close();
+		
+		// call the web service method to get the entity we just added 
+		Territory updatedTerritory = territoryResource.path("{id}").resolveTemplate("id", newTerritoryId).request().get(Territory.class);
+		assertNotNull(updatedTerritory);
+		assertEquals(addedTerritory.getTerritoryID(), updatedTerritory.getTerritoryID());
+		assertEquals(addedTerritory.getTerritoryDescription(), updatedTerritory.getTerritoryDescription());
+		
+		// call the RESTful web service to delete the region entity
+		response = territoryResource.path("{id}").resolveTemplate("id", newTerritoryId).request(MediaType.APPLICATION_JSON).delete();
+		assertEquals(200, response.getStatus());
+		response.close();		
 		
 	}
 
